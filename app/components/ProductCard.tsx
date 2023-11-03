c4e1d79062ebdaf490867b468e80643058aea66e
jasongriller <jgriller@knights.ucf.edu>
2023-11-03 10:12:55 -0400

    Added some functionality
    Add to cart button

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import AddToCart from './AddToCart'
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <AddToCart />
+    </div>
+  )
+}
+export default ProductCard
